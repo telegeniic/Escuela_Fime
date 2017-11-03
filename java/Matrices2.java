84fc8d7442960c93cf01c80921485ee1918fe25f
Martin Guajardo <martinguajardo20@gmail.com>
2017-11-03 17:54:03 -0600

    Empieza clase del 3/11

---
+++ java/Matrices2.java
@@ -0,0 +1,13 @@
+/*
+Nombre: Jesus Martin Guajardo Olivo
+Matricula: 1838264
+Hora: N2
+Descripcion: Generar numeros nones
+*/
+import javax.swing.JOptionPane;
+public class Matrices2{
+	public static void main(String[] args) {
+		String entrada;
+
+	}//Fin del metodo main
+}//Fin de la clase
